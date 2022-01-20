b9ad7ade6209cb3b07e4f9bb3963911467c7f901
williamyhg1 <90809764+williamyhg1@users.noreply.github.com>
2022-01-20 21:10:52 +1100

    add background

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
-import "./App.css";
+import "./index.css";
 
 const App = () => {
   const apiKey = "aec9fee4a26accaeb55d650fc82fbd07";
-  const [weatherData, setWeatherData] = useState([{}]);
+  const [weatherData, setWeatherData] = useState({});
   const [city, setCity] = useState("");
   const getWeather = (event) => {
     if (event.key === "Enter") {
@@ -13,39 +13,43 @@ const App = () => {
         .then((response) => response.json())
         .then((data) => {
           setWeatherData(data);
+          setCity("");
         });
     }
   };
   return (
-    <div className="container">
-      <p className="title">Weather app</p>
-      <input
-        className="input"
-        placeholder="Enter City Name..."
-        onChange={(e) => setCity(e.target.value)}
-        value={city}
-        onKeyPress={getWeather}
-      />
-
-      {typeof weatherData.main === "undefined" ? (
-        <div>
-          <p className="reminder">
-            Welcome to weather app! Enter a city name to start...
-          </p>
+    <div className="app">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeholder="Search..."
+            onChange={(e) => setCity(e.target.value)}
+            value={city}
+            onKeyPress={getWeather}
+          />
         </div>
-      ) : (
-        <div className="weather-data">
-          <p className="city">{weatherData.name}</p>
-          <p className="temp">{Math.round(weatherData.main.temp)}°C</p>
-          <p className="weather">{weatherData.weather[0].main}</p>
-        </div>
-      )}
-      
-      {weatherData.cod === '404'? (
-        <p className="error">City not found! Please try again...</p>
-      ):(<></>)}
+        {typeof weatherData.main === "undefined" ? (
+          <div>
+            <p className="welcome-message">
+              Welcome to weather app! Enter a city name to start...
+            </p>
+          </div>
+        ) : (
+          <div className="weather-data">
+            <p className="city">{weatherData.name}</p>
+            <p className="temp">{Math.round(weatherData.main.temp)}°C</p>
+            <p className="weather">{weatherData.weather[0].main}</p>
+          </div>
+        )}
 
-
+        {weatherData.cod === "404" ? (
+          <p className="error">City not found! Please try again...</p>
+        ) : (
+          <></>
+        )}
+      </main>
     </div>
   );
 };
